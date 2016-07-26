720dc7fea7c0c60d16d317e46b3999b9d9c257bc
xsu <xsu@thoughtworks.com>
2016-07-26 22:22:52 +0800

    add checkout movie logic.

---
+++ src/main/java/twu/biblioteca/control/CheckoutMovieLogic.java
@@ -0,0 +1,51 @@
+package twu.biblioteca.control;
+
+import twu.biblioteca.model.collection.BookCollection;
+import twu.biblioteca.model.collection.MovieCollection;
+import twu.biblioteca.model.entity.Book;
+import twu.biblioteca.model.entity.Entity;
+import twu.biblioteca.model.entity.Movie;
+import twu.biblioteca.view.UIEvent;
+import twu.biblioteca.view.UIEventCallBack;
+import twu.biblioteca.view.UIThread;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * Created by xsu on 16/7/26.
+ * it's the logic of checkout movie
+ */
+public class CheckoutMovieLogic implements LogicNode {
+    @Override
+    public void action() throws Exception {
+        UIThread.getUiThread().addEvent(new UIEvent("Please input movie id.", new UIEventCallBack() {
+            @Override
+            public void action() {
+                Scanner scanner = new Scanner(System.in);
+                String value = scanner.next();
+                getInputMessage(value);
+            }
+        }));
+    }
+
+    public void getInputMessage(String value) {
+        Map<String, Object> filter = new HashMap<>();
+        filter.put(Entity.ENTITY_ID_FIELD, value);
+        filter.put(Movie.MOVIE_IS_CHECKOUT_FIELD, false);
+        List<Movie> result = MovieCollection.getMovieCollection().findData(filter);
+        for (Movie now : result) {
+            now.setISCheckout(true);
+            MovieCollection.getMovieCollection().updateData(now);
+        }
+
+        if (!result.isEmpty()) {
+            UIThread.getUiThread().addEvent(new UIEvent("Thank you! Enjoy the movie"));
+        } else {
+            UIThread.getUiThread().addEvent(new UIEvent("That movie is not available."));
+        }
+        ControlThread.getControlThread().addEvent(MainMenuLogic.class);
+    }
+}
---
+++ src/test/java/twu/biblioteca/control/CheckoutMovieLogicTest.java
@@ -0,0 +1,90 @@
+package twu.biblioteca.control;
+
+import org.junit.Test;
+import twu.biblioteca.model.collection.MovieCollection;
+import twu.biblioteca.model.entity.Movie;
+import twu.biblioteca.view.UIThread;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by xsu on 16/7/26.
+ * it's the testing code for checkout movie
+ */
+public class CheckoutMovieLogicTest extends LogicTesing {
+
+    @Test
+    public void should_add_check_out_hint_message() throws Exception {
+        CheckoutMovieLogic checkoutMovieLogic = new CheckoutMovieLogic();
+        checkoutMovieLogic.action();
+
+        assertEquals("Please input movie id.", UIThread.getUiThread().getNextUIEvent().getMessage());
+    }
+
+    @Test
+    public void should_back_to_main_menu_after_input() throws Exception {
+        CheckoutMovieLogic checkoutMovieLogic = new CheckoutMovieLogic();
+        checkoutMovieLogic.getInputMessage("1");
+
+        assertEquals(MainMenuLogic.class, ControlThread.getControlThread().getNextEvent());
+    }
+
+    @Test
+    public void should_mark_movie_as_is_checkout_when_select_a_id() throws Exception {
+        MovieCollection movieCollection = MovieCollection.getMovieCollection();
+        for (int i = 0; i < 10; i++) {
+            movieCollection.insertData(new Movie(Integer.toString(i), "movie-" + i, "1999", "director-" + i / 2, 1, false));
+        }
+
+        CheckoutMovieLogic checkoutMovieLogic = new CheckoutMovieLogic();
+        checkoutMovieLogic.getInputMessage("1");
+
+        Map<String, Object> filter = new HashMap<>();
+        filter.put(Movie.MOVIE_IS_CHECKOUT_FIELD, true);
+        assertEquals(1, movieCollection.findData(filter).size());
+        assertEquals("movie-1", movieCollection.findData(filter).get(0).getMovieName());
+    }
+
+    @Test
+    public void should_add_success_message_if_success() throws Exception {
+        MovieCollection movieCollection = MovieCollection.getMovieCollection();
+        for (int i = 0; i < 10; i++) {
+            movieCollection.insertData(new Movie(Integer.toString(i), "movie-" + i, "1999", "director-" + i / 2, 1, false));
+        }
+
+        CheckoutMovieLogic checkoutMovieLogic = new CheckoutMovieLogic();
+        checkoutMovieLogic.getInputMessage("1");
+
+        assertEquals("Thank you! Enjoy the movie", UIThread.getUiThread().getNextUIEvent().getMessage());
+    }
+
+    @Test
+    public void should_show_fail_message_if_failed() throws Exception {
+        MovieCollection movieCollection = MovieCollection.getMovieCollection();
+        for (int i = 0; i < 10; i++) {
+            movieCollection.insertData(new Movie(Integer.toString(i), "movie-" + i, "1999", "director-" + i / 2, 1, false));
+        }
+
+        CheckoutMovieLogic checkoutMovieLogic = new CheckoutMovieLogic();
+        checkoutMovieLogic.getInputMessage("1000");
+
+        assertEquals("That movie is not available.", UIThread.getUiThread().getNextUIEvent().getMessage());
+    }
+
+    @Test
+    public void should_not_checkout_book_who_is_already_checkout() throws Exception {
+        MovieCollection movieCollection = MovieCollection.getMovieCollection();
+        for (int i = 0; i < 10; i++) {
+            movieCollection.insertData(new Movie(Integer.toString(i), "movie-" + i, "1999", "director-" + i / 2, 1, true));
+        }
+
+        CheckoutMovieLogic checkoutMovieLogic = new CheckoutMovieLogic();
+        checkoutMovieLogic.getInputMessage("1");
+
+        assertEquals("That movie is not available.", UIThread.getUiThread().getNextUIEvent().getMessage());
+    }
+
+}
